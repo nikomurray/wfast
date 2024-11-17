269710c9bbb9c5eaebb050a6db701fb6f6c94589
Nicolas <npereyramurray@gmail.com>
2024-11-17 19:37:50 -0300

    a

--- src/components/Qr.jsx
+++ src/components/Qr.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { AppContext } from "../App";
-
+import CircularProgress from '@mui/material/CircularProgress';
 export default function Qr() {
   const { qrCode } = useContext(AppContext);
 
@@ -8,7 +8,7 @@ export default function Qr() {
     <div className="qr-container">
       <p>Vinculate your whatsapp account</p>
       <div className="qr-image">
-        {qrCode ? <img src={qrCode} alt="qrcode" /> : <p>Loading QR Code...</p>}
+        {qrCode ? <img src={qrCode} alt="qrcode" /> : <div className="loading-qr"><CircularProgress size="60px" /> <p>Loading QR Code...</p></div>}
       </div>
     </div>
   );
