9874ef1c6ebda707245fdcc91a580a906ad1dd1c
Nicolas <npereyramurray@gmail.com>
2024-11-21 20:57:30 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext();
 
 export default function App() {
   const [qrCode, setQrCode] = useState(null);
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
   const [isSendingMessages, setIsSendingMessages] = useState(false);
   const [messageData, setMessageData] = useState({
     message: "",
